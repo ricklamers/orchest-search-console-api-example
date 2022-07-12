d7d7242bc6d10bbe090d082f06d8b4a7c59f7525
Rick Lamers <ricklamers@gmail.com>
2022-07-12 08:34:28 +0000

    Init

---
+++ .orchest/environments/60445ec4-767d-46be-a872-534de832f4ee/setup_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install any dependencies you have in this shell script,
+# see https://docs.orchest.io/en/latest/fundamentals/environments.html#install-packages
+
+# E.g. mamba install -y tensorflow
+
+pip install -r requirements.txt
